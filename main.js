71115b4c852a75747b6115751ac3f25d036b6d69
SoQu-PJ <maniek999zano@gmail.com>
2023-01-16 22:52:10 +0100

    Fix bugs and replaced ul to form

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 const THANK_YOU_SECTION = document.querySelector(".thank-you-state-rating");
 const RATING_SECTION = document.querySelector(".rating-state-start");
 const SUBMIT = document.querySelector(".submit");
-const RATING_LIST = document.querySelectorAll(".rating li");
+const RATING_LIST = document.querySelectorAll(".rating label");
 let thankYouRating = document.querySelector(".submit-rating");
 
-let rating = 0;
+// Find radio value
+function findSelection(field) {
+    var test = document.getElementsByName(field);
+    var sizes = test.length;
+    for (i = 0; i < sizes; i++) {
+        if (test[i].checked == true) {
+            return test[i].value;
+        }
+    }
+}
 
 RATING_LIST.forEach((el, index) => {
     let prevElem = index == 0 ? 0 : RATING_LIST[index - 1];
-    // Get rating and Animation to select rating
-    el.addEventListener("click", () => {
-        // Class on only one elements
-        (document.querySelector(".is-selected-rating")) ? document.querySelector(".is-selected-rating").classList.remove("is-selected-rating"): "";
-
-        // Selected rating animation
-        el.classList.add("is-selected-rating");
-
-        // Get rating
-        rating = el.textContent;
-    })
     // Animation Rating mouseOver/mouseOut START
     el.addEventListener("mouseover", () => {
         if (index > 0)
@@ -34,13 +32,14 @@ RATING_LIST.forEach((el, index) => {
 })
 
 // Submit Button Animation and Submitting a rating START
-SUBMIT.addEventListener("click", () => {
+SUBMIT.addEventListener("click", (e) => {
+    e.preventDefault();
+    // Sumbit rating
+    thankYouRating.innerHTML = findSelection("rating");
+
     // Hidden rating_section 
     RATING_SECTION.style.animation = "hiddenRatingSection 0.5s"
     RATING_SECTION.addEventListener("animationend", () => {
-        // Submitting a rating
-        thankYouRating.innerHTML = rating;
-
         // Show thank_you_section 
         THANK_YOU_SECTION.style.animation = "visibleRatingSection 0.5s"
         THANK_YOU_SECTION.classList.remove("hidden");
